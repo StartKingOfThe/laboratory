25e4047936cc163bdefe5bf7ed6d2e9a23cfa026
user-maxim-name <maksym.pitula-pi1901@kep.nung.edu.ua>
2022-12-23 16:07:13 +0200

    show current date

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorldGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now.ToString());
         }
     }
 }
