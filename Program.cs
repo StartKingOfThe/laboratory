e0a55d950af51b8dd00c096cb1008c59f75bcee0
user-maxim-name <maksym.pitula-pi1901@kep.nung.edu.ua>
2022-12-23 19:36:15 +0200

    add input data ability

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
+using System.Text;
 namespace HelloWorldGit
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DateTime.Now.ToString());
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Введiть ваше iм'я\n>>>");
+            String name = Console.ReadLine();
+            Console.WriteLine("Привет " + name);
+
         }
     }
 }
