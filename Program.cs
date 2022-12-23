27022ec0aaf8350bc9139fdda7f0b2f006a6a2cc
user-maxim-name <maksym.pitula-pi1901@kep.nung.edu.ua>
2022-12-23 20:29:03 +0200

    added read last date and write current date to file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text;
+
+using System.IO;
 namespace HelloWorldGit
 {
     class Program
@@ -7,9 +9,36 @@ namespace HelloWorldGit
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.Write("Введiть ваше iм'я\n>>>");
-            String name = Console.ReadLine();
-            Console.WriteLine("Привет " + name);
+
+            string text = "";
+
+            if (new FileInfo("times.txt").Length == 0)
+
+            {
+                Console.WriteLine("File is empty");
+
+
+            }
+            else {
+
+                StreamReader reader = new StreamReader("times.txt");
+                
+                while (reader.Peek() != -1)
+                {
+
+                    text = reader.ReadLine();
+
+                }
+                reader.Close();
+                Console.WriteLine("Last date: " + text);
+            }
+             
+            StreamWriter writer = new StreamWriter("times.txt", true);
+            writer.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm"));
+
+            writer.Close();
+
+
 
         }
     }
